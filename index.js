cf27934b117ab31260d611dada6fe70131a206fa
davidgeorgebell <bell.dave93@gmail.com>
2019-12-27 09:17:18 +0800

    request for individual contacts, if contact doesnt exist responds with status 404

--- index.js
+++ index.js
@@ -35,11 +35,22 @@ console.log(currentDate);
 app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
+
 app.get('/info', (req, res) => {
   res.send(
     `<p>Phonebook has info for ${persons.length} people.</p> 
     <p>${currentDate()}</p>`
   );
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 
 const PORT = 3001;
